78ebda6fbd542781cb7f67b82e897075ee6af64e
Dmytro <fantaigr69@gmail.com>
2023-01-22 02:05:53 +0200

    feat: drag and drop files

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -3,20 +3,21 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Пошук фігур</title>
-    <script type="text/javascript" src="jquery-3.3.1.min.js"></script>
-    @vite(['resources/css/file-input.css'])
+    <title>Finding figures</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    @vite(['resources/css/file-input.css', 'resources/js/file-input.js'])
 </head>
 
 <body>
     <form id="upload-container" method="POST" action="send.php">
-        <img id="upload-image" src="https://cdn-icons-png.flaticon.com/512/2716/2716054.png">
+        <img id="upload-image" src="https://habrastorage.org/webt/dr/qg/cs/drqgcsoh1mosho2swyk3kk_mtwi.png">
         <div>
             <input id="file-input" type="file" name="file" multiple>
-            <label for="file-input">Виберіть файл</label>
-            <span>або перенесіть його сюди</span>
+            <label for="file-input">Select a file</label>
+            <span>or drag it here</span>
         </div>
     </form>
+
 </body>
 
 </html>
--- app/vite.config.js
+++ app/vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/file-input.css'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/file-input.css', 'resources/js/file-input.js'],
             refresh: true,
         }),
     ],
