ea51be003cdf059a8594d854f370bbbf02c857b1
Dmytro <fantaigr69@gmail.com>
2023-01-22 21:15:04 +0200

    feat: load file methods

--- app/app/Http/Controllers/FileUploadController.php
+++ app/app/Http/Controllers/FileUploadController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class FileUploadController extends Controller
 {
@@ -15,15 +16,104 @@ class FileUploadController extends Controller
 
         if ($file = $req->file('file')) {
 
-            echo "File has been uploaded. File size: " . $file->getSize() . " bytes";
+            echo "File has been uploaded. File size: " . $file->getSize() . " bytes <br>";
 
-            $bytes = file_get_contents($file->getRealPath());
+            // $image = imagecreatefrompng($file);
+
+            // echo "Image has been created <br>";
+
+            // imagebmp($image, null, false);
+
+            // $samples = [];
+            // $labels = [];
+
+            // $triangles = $this->loadAllFromStorage('triangles');
+            // $circles = $this->loadAllFromStorage('circles');
+            // $squares = $this->loadAllFromStorage('squares');
+
+            // $i = 0;
+            // foreach ($triangles as $key => $img) {
+            //     $bytes = $this->loadFromStorage($img);
+            //     $integers = $this->convertBytesToIntegerArray($bytes);
+            //     $samples[$i + $key][] = $integers;
+            //     $labels[$i + $key][] = 'triangle';
+            //     echo count($integers) . " ";
+            // }
+
+            // foreach ($circles as  $key => $img) {
+            //     $bytes = $this->loadFromStorage($img);
+            //     $integers = $this->convertBytesToIntegerArray($bytes);
+            //     $samples[$i + $key][] = $integers;
+            //     $labels[$i + $key][] = 'circle';
+            // }
+
+            // foreach ($squares as $key => $img) {
+            //     $bytes = $this->loadFromStorage($img);
+            //     $integers = $this->convertBytesToIntegerArray($bytes);
+            //     $samples[$i + $key][] = $integers;
+            //     $labels[$i + $key][] = 'square';
+            // }
+            // echo "<br>Training data loaded";
+
+            // foreach ($triangles as $key => $p) {
+            //     $img = imagecreatefrompng($p);
+            //     $image = imagescale($img, 256, 256);
+            //     imagepng($image, $file->path());
+            // }
+
+
+            // $bytes = file_get_contents($file);
+            // // $array = unpack("c2/n", $bytes);
+            // // print_r($array);
+            // $targetIntegers = $this->convertBytesToIntegerArray($bytes);
+            // dd($bytes);
+            // dd($targetIntegers);
+            // return;
+            // $classifier = new KNearestNeighbors();
+
+            // $classifier->train($samples, $labels);
+            // $classifier->train($samples, $labels);
+            // $classifier->train($samples, $labels);
+            // $classifier->train($samples, $labels);
+
+
+            // return $classifier->predict($targetIntegers);
         }
+    }
+
+    function getArrayIntegersFromDirectory($directoryName)
+    {
+        $files = $this->loadAllFromStorage($directoryName);
+        $integers = [];
+        foreach ($files as $file) {
+            $bytes = $this->loadFromStorage($file);
+            $integers[] = $this->convertBytesToIntegerArray($bytes);
+        }
+        return $integers;
     }
 
     function saveToStorage(UploadedFile $file)
     {
         $fileName = time() . '_' . $file->getClientOriginalName();
         $file->file('file')->storeAs('uploads', $fileName, 'public');
+    }
+
+    function loadFromStorage($fileName)
+    {
+        return Storage::disk('local')->get($fileName);
+    }
+
+    function loadAllFromStorage($directoryName)
+    {
+        return Storage::files('ai-resources/' . $directoryName);
+    }
+
+    function convertBytesToIntegerArray($bytes)
+    {
+        $integers = [];
+        for ($i = 0; $i < strlen($bytes); $i++) {
+            $integers[] = ord($bytes[$i]);
+        }
+        return $integers;
     }
 }
